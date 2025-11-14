574a2a18e6479bcd12e62feefa49998059366258
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-14 22:30:42 +0000

    Revert "gacha.js doesn't need character import"
    This reverts commit f621de2fea54968c65466e57942c6083ea8dce61.

--- scripts/gacha.js
+++ scripts/gacha.js
@@ -1,3 +1,6 @@
+import { characters, Character} from './chars.js';
+
+
 const class_names = {
     sparkle: "sparkle",
     character_card: "character-card",
@@ -13,29 +16,6 @@ const result_elements = Array.from(document.getElementsByClassName("result-conta
         new_character: p.getElementsByClassName(class_names.new_character)[0]
     }
 }).sort((l, r) => l.id - r.id);
-
-
-const character = characters.find((c) => c.name === "Little Mermaid");
-const pull_assets_url = "images/pull_assets";
-
-result_elements.forEach((e) => {
-    e.character_card.src = character.characterCardImage;
-    e.character_card.title = character.name;
-    e.new_character.src = `${pull_assets_url}/new_character.png`;
-    e.spare_body.src = character.characterSpareBodyImage;
-    e.spare_body.title = `${character.name}'s Spare Body`;
-
-    if (character.rarity === Character.Rarities.SR) {
-        e.character_card.classList.add(e.id <= 5 ? "sr-top" : "sr-bottom");
-        e.sparkle.src = `${pull_assets_url}/glow_sr.png`;
-    }
-    else if (character.rarity === Character.Rarities.SSR) {
-        e.character_card.classList.add(e.id <= 5 ? "ssr-top" : "ssr-bottom");
-        e.sparkle.src = `${pull_assets_url}/glow_ssr.png`;
-    } 
-    else
-        e.sparkle.src = `${pull_assets_url}/glow_none.png`;
-});
 
 
 export { result_elements };
