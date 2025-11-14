d88f65dd9bfb7b748dd65c8efec775e776a4c017
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-14 22:28:41 +0000

    Create gacha.js
    Script for all gacha pages to use.
    Hooks the elements that display character cards for manipulation via importing scripts.

---
+++ scripts/gacha.js
@@ -0,0 +1,21 @@
+import { characters, Character} from './chars.js';
+
+
+const class_names = {
+    sparkle: "sparkle",
+    character_card: "character-card",
+    spare_body: "spare-body",
+    new_character: "new-character"
+};
+const result_elements = Array.from(document.getElementsByClassName("result-container")).map((p) => {
+    return {
+        id: parseInt(p.id.substring(p.id.indexOf("-") + 1)),
+        sparkle: p.getElementsByClassName(class_names.sparkle)[0],
+        character_card: p.getElementsByClassName(class_names.character_card)[0],
+        spare_body: p.getElementsByClassName(class_names.spare_body)[0],
+        new_character: p.getElementsByClassName(class_names.new_character)[0]
+    }
+}).sort((l, r) => l.id - r.id);
+
+
+export { result_elements };
