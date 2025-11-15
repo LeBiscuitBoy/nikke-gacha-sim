ccd97951924c7f3aa58f21845046131481f2ba75
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-15 12:18:40 +0000

    Roll functions
    Added functions for special and standard pulls.
    Special pulls are not yet implemented.

--- scripts/gacha.js
+++ scripts/gacha.js
@@ -1,5 +1,5 @@
 import { Character, characters } from './chars.js';
-import { setWishList, characterOnWishlist } from './savedata.js';
+import { characterOnWishlist } from './savedata.js';
 
 
 const result_elements = Array.from(document.getElementsByClassName("result-container")).map((p) => {
@@ -23,4 +23,51 @@ const roundToNearestHalf = (num) => {
     return flooredNum;
 }
 
-export { result_elements };
+function special_pull(rate_up_character, is_ten_pull = true) {
+    return "NOT YET IMPLEMENTED";
+}
+
+function standard_pull(is_ten_pull = true) {
+    const pool_filter = (character, rarity) => character.isInStandardPool && character.rarity === rarity;
+    const is_special_character = (character) => character.manufacturer === Character.Manufacturers.Pilgrim || character.overspec;
+    const pools = {
+        R: {
+            Characters: characters.filter((c) => pool_filter(c, Character.Rarities.R)),
+            Rate: 53
+        },
+        SR: {
+            Characters: characters.filter((c) => pool_filter(c, Character.Rarities.SR)),
+            Rate: 43
+        },
+        SSR: {
+            Characters: characters.filter((c) => pool_filter(c, Character.Rarities.SSR) && characterOnWishlist(c.name) && !is_special_character(c)),
+            Rate: 3.5
+        },
+        Special: {
+            Characters: characters.filter((c) => pool_filter(c, Character.Rarities.SSR) && characterOnWishlist(c.name) && is_special_character(c)),
+            Rate: 0.5
+        }
+    };
+    const get_character = (roll) => {
+        if (roll <= pools.R.Rate)
+            return getRandomElementFromArray(pools.R.Characters);
+        else if (roll <= (pools.R.Rate + pools.SR.Rate))
+            return getRandomElementFromArray(pools.SR.Characters);
+        else if (roll <= (pools.R.Rate + pools.SR.Rate + pools.SSR.Rate))
+            return getRandomElementFromArray(pools.SSR.Characters);
+        else 
+            return getRandomElementFromArray(pools.Special.Characters);
+    };
+
+
+    if (!is_ten_pull)
+        return get_character(roundToNearestHalf(Math.random() * 100));
+
+    let pulls = [];
+    for (let r = 0; r < 10; r++)
+        pulls.push(get_character(roundToNearestHalf(Math.random() * 100)));
+    return pulls;
+}
+
+
+export { result_elements, standard_pull, special_pull };
