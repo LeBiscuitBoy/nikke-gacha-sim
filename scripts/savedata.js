92392bea6710c9ca5504e2df15a37cd323c62c51
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-15 13:03:16 +0000

    Get pulled character list from save data

--- scripts/savedata.js
+++ scripts/savedata.js
@@ -23,6 +23,9 @@ function setWishList(character_object) {
 function getWishList() { 
     return JSON.parse(localStorage.getItem(keys.wishedCharacters));
 }
+function getPullList() {
+    return JSON.parse(localStorage.getItem(keys.pulledCharacters));
+}
 
 function characterInList(character_name, list) {
     const index_of_char = list.findIndex((cn) => cn === character_name);
@@ -37,17 +40,17 @@ function characterInList(character_name, list) {
 function characterOnWishlist(character_name) {
     const wl = getWishList();
     return characterInList(character_name, [
-        ...wl.pilgrim, 
-        ...wl.elysion, 
-        ...wl.missilis, 
-        ...wl.tetra
+        ...wl.Pilgrim, 
+        ...wl.Elysion, 
+        ...wl.Missilis, 
+        ...wl.Tetra
     ]).state;
 }
 
 function hasCharacter(character_name) {
     const character_list = JSON.parse(
         localStorage.getItem(keys.pulledCharacters)).map((c) => c.name);
-    return characterInList(character_name, character_list);
+    return characterInList(character_name, character_list).state;
 }
 
 
@@ -72,5 +75,6 @@ export {
     hasCharacter, 
     getWishList, 
     setWishList,
-    characterOnWishlist
+    characterOnWishlist,
+    getPullList
 };
