ea1ab3574b331b37e6f955f825260402652b3f16
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-15 13:22:31 +0000

    Script for setting character detail on page

--- scripts/script.js
+++ scripts/script.js
@@ -1,101 +1,66 @@
-import { characters, Character} from './chars.js';
-import { addCharacterToCollection, hasCharacter, getWishList, setWishList } from './savedata.js';
+import { Character } from './chars.js';
+import { result_elements, standard_pull  } from './gacha.js';
+import { hasCharacter, addCharacterToCollection, setWishList, getPullList } from './savedata.js';
 
-const timeout_delay = 1200;
-setTimeout(() => {
-    document.getElementById("options").hidden = false;
-    document.getElementById("tickets-gained-box").style.visibility = "visible";
-}, timeout_delay);
+
+function setElementDetail(element, character) {
+    const asset_url_prefix = "images/pull_assets";
+    const char_url_prefix = "images/character";
+
+    function getGlowAsset() {
+        if (character.rarity === Character.Rarities.R)
+            return `${asset_url_prefix}/glow_none.png`;
+        else if (character.rarity === Character.Rarities.SR)
+            return `${asset_url_prefix}/glow_sr.png`;
+        else
+            return `${asset_url_prefix}/glow_ssr.png`;
+    }
+
+    element.sparkle.src = getGlowAsset();
+    element.character_card.src = `${char_url_prefix}/${character.characterCardImage}`;
+    element.character_card.title = character.name;
+    
+    if (character.rarity !== Character.Rarities.R) {
+        const addClass = (top, bottom) => element.character_card.classList.add(element.id <= 5 ? top : bottom);
+        
+        if (character.rarity === Character.Rarities.SR)
+            addClass("sr-top", "sr-bottom");
+        else
+            addClass("ssr-top", "ssr-bottom");
+    }
+
+    if (hasCharacter(character.name)) {
+        element.spare_body.src = `${char_url_prefix}/${character.characterSpareBodyImage}`;
+        element.spare_body.title = `${character.name}'s Spare Body`;
+
+        element.new_character.src = "";
+        element.new_character.title = "";
+    }
+    else {
+        element.spare_body.src = `${asset_url_prefix}/new_body_empty.png`;
+        element.spare_body.title = "";
+        
+        element.new_character.src = `${asset_url_prefix}/new_character.png`;
+        element.new_character.title = "New!";
+    }
+};
 
 
 // Temporary section for programmatically adding characters to the wishlist.
 setWishList({
-    pilgrim:  ["Little Mermaid", "Rapi: Red Hood", "Red Hood", "Grave", "Cinderella"],
-    elysion:  ["Emma: Tactical Upgrade", "Eunhwa: Tactical Upgrade", "Vesti: Tactical Upgrade", "Privaty", "D: Killer Wife"],
-    missilis: ["Elegg", "Maxwell", "Drake", "Laplace", "Mana"],
-    tetra:    ["Blanc", "Noir", "Bready", "Crust", "Rupee"]
+    Pilgrim:  ["Little Mermaid", "Rapi: Red Hood", "Red Hood", "Grave", "Cinderella"],
+    Elysion:  ["Emma: Tactical Upgrade", "Eunhwa: Tactical Upgrade", "Vesti: Tactical Upgrade", "Privaty", "D: Killer Wife"],
+    Missilis: ["Elegg", "Maxwell", "Drake", "Laplace", "Mana"],
+    Tetra:    ["Blanc", "Noir", "Bready", "Crust", "Rupee"]
 });
 
-const wl = getWishList();    
-[...wl.pilgrim, ...wl.elysion, ...wl.missilis, ...wl.tetra].forEach((wc) =>
-    characters.find((c) => c.name === wc).wishlisted = true
-);
 
 
-const standardPool = characters.filter((c) => 
-    (c.isInStandardPool && c.rarity === Character.Rarities.SSR && c.wishlisted) || 
-    (c.isInStandardPool && c.rarity !== Character.Rarities.SSR));
+const pulls = standard_pull(true);
+for (let i = 0; i < 10; i++) {
+    const element = result_elements[i];
+    const character = pulls[i];
 
-const charsR = standardPool.filter((c) => c.rarity === Character.Rarities.R);
-const charsSR = standardPool.filter((c) => c.rarity === Character.Rarities.SR);
-const charsSSR = standardPool.filter((c) => c.rarity === Character.Rarities.SSR && !c.overspec && c.manufacturer !== Character.Manufacturers.Pilgrim);
-const charsSpecialSSR = standardPool.filter((c) => c.rarity === Character.Rarities.SSR && (c.manufacturer === Character.Manufacturers.Pilgrim || c.overspec));
-
-
-const roundToNearestHalf = (num) => {
-    const flooredNum = Math.floor(num);
-    const decimalPortion = Math.abs(num - flooredNum);
-
-    if (decimalPortion > 0.75) return flooredNum + 1;
-    if (decimalPortion > 0.25) return flooredNum + 0.5;
-    return flooredNum;
+    setElementDetail(element, character);
+    addCharacterToCollection(character.name);
 }
-const getRandomIndexFromArray = (arr) => Math.floor(Math.random() * arr.length);
-
-function gamble() {
-    const rateR = 53;
-    const rateSR = 43;
-    const rateSSR = 3.5;
-    let rolledCharacters = [];
-
-    for (let i = 0; i < 10; i++) {
-        const roll = roundToNearestHalf(Math.random() * 100);
-
-        if (roll <= rateR)
-            rolledCharacters.push(charsR[getRandomIndexFromArray(charsR)]);
-        else if (roll <= (rateR + rateSR))
-            rolledCharacters.push(charsSR[getRandomIndexFromArray(charsSR)]);
-        else if (roll <= (rateR + rateSR + rateSSR))
-            rolledCharacters.push(charsSSR[getRandomIndexFromArray(charsSSR)]);
-        else 
-            rolledCharacters.push(charsSpecialSSR[getRandomIndexFromArray(charsSpecialSSR)]);
-    }
-    return rolledCharacters;
-}
-
-
-let i = 1;
-gamble().forEach((c) => {
-    const image_dir = "/images/pull_assets/";
-
-    const result_container = document.getElementById(`result-${i}`);
-    const char_image = result_container.getElementsByClassName("char-img")[0];
-    const new_char = result_container.getElementsByClassName("new-char-img")[0];
-    const spare_body = result_container.getElementsByClassName("spare-body-label")[0];
-    const sparkle = result_container.getElementsByClassName("sparkle")[0];
-
-    char_image.src = c.characterImage;
-    char_image.setAttribute("title", c.name);
-    char_image.setAttribute("alt", c.name);
-
-    const top = i++ < 6;
-    if (c.rarity === Character.Rarities.SSR) {
-        char_image.classList.add(top ? "ssr-top" : "ssr-bottom");
-        sparkle.src = `${image_dir}glow_ssr.png`;
-    }
-    else if (c.rarity === Character.Rarities.SR) {
-        char_image.classList.add(top ? "sr-top" : "sr-bottom");
-        sparkle.src = `${image_dir}glow_sr.png`;
-    }
-
-    if (!hasCharacter(c.name).state)
-        setTimeout(() => 
-            new_char.src = `${image_dir}new_character.png`, timeout_delay);
-    else
-        setTimeout(() => {
-            //spare_body.src = `${image_dir}${c.characterSpareBodyImage}`;
-            spare_body.src = `${image_dir}spare_body_temp.png`;
-        }, timeout_delay);
-
-    addCharacterToCollection(c.name);
-});
