236f5189569f44d63b851ddb7df371b36de0a829
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-15 11:50:54 +0000

    Update gacha.js

--- scripts/gacha.js
+++ scripts/gacha.js
@@ -1,16 +1,14 @@
-const class_names = {
-    sparkle: "sparkle",
-    character_card: "character-card",
-    spare_body: "spare-body",
-    new_character: "new-character"
-};
+import { Character, characters } from './chars.js';
+import { setWishList, characterOnWishlist } from './savedata.js';
+
+
 const result_elements = Array.from(document.getElementsByClassName("result-container")).map((p) => {
     return {
         id: parseInt(p.id.substring(p.id.indexOf("-") + 1)),
-        sparkle: p.getElementsByClassName(class_names.sparkle)[0],
-        character_card: p.getElementsByClassName(class_names.character_card)[0],
-        spare_body: p.getElementsByClassName(class_names.spare_body)[0],
-        new_character: p.getElementsByClassName(class_names.new_character)[0]
+        sparkle: p.getElementsByClassName("sparkle")[0],
+        character_card: p.getElementsByClassName("character-card")[0],
+        spare_body: p.getElementsByClassName("spare-body")[0],
+        new_character: p.getElementsByClassName("new-character")[0]
     }
 }).sort((l, r) => l.id - r.id);
 
