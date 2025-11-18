740b67f0d3d0e04cde1e0656ef31e505a745dafa
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-18 20:36:47 +0000

    Tab navigation controller

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -2,22 +2,48 @@ import { Character } from './chars.js';
 
 
 const banners = [
-    "Nayuta",   
     "Liberalio",
+    "Nayuta",   
+    "Delta: Ninja Thief",
+    "Ada",
+    "Jill",
 ]
 function setImage(img, url, title) {
     img.src = url,
     img.title = title;
 }
 
-const newest_banner = banners[0];
-const sanitized_name = newest_banner.toLowerCase();
-Character.IllegalImageChars.forEach((c) => sanitized_name.replaceAll(c, ""));
 
+const newest_banner = banners[0];
+let sanitized_name = newest_banner.toLowerCase();
+Character.IllegalImageChars.forEach((c) => sanitized_name = sanitized_name.replaceAll(c, ""));
 
 document.getElementById("current-banner-title").innerText = newest_banner.toUpperCase();
 setImage(document.getElementById("current-banner-image"), `images/banners/${sanitized_name}.png`, `Rate Up: ${newest_banner}`)
 
+
+{
+    function toggleElement(element, is_visible) {
+        element.style.display = (is_visible ? "block" : "none");
+    }
+
+    const current_banner_element = document.getElementById("current-banner-container");
+    const previous_banner_element = document.getElementById("previous-banners-container");
+
+    document.getElementById("current-banner-button").addEventListener("click", () => {
+        toggleElement(current_banner_element, true);
+        toggleElement(previous_banner_element, false);
+    });
+    document.getElementById("previous-banners-button").addEventListener("click", () => {
+        toggleElement(current_banner_element, false);
+        toggleElement(previous_banner_element, true);
+    });
+    document.getElementById("standard-banner-button").addEventListener("click", () => {
+        toggleElement(current_banner_element, false);
+        toggleElement(previous_banner_element, false);
+    });
+    toggleElement(previous_banner_element, false);
+}
 
 document.getElementById("rate-up-single-pull-button").addEventListener("click", () => {
     window.location.replace(`gacha.html?character=${sanitized_name}&singlepull`);
