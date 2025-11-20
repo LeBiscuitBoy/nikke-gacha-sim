f3a44b139fb089e670ee8c1134b0592398af6154
Biscuit Boy <78423562+LeBiscuitBoy@users.noreply.github.com>
2025-11-20 00:18:08 +0000

    Added wishlist selector
    Styling needs to be refactored

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -34,11 +34,10 @@ const banners = characters.filter((c) => c.hasBanner).map((c) => c.name);
 }
 
 
+function toggleElement(element, is_visible) {
+    element.style.display = (is_visible ? "block" : "none");
+}
 {
-    function toggleElement(element, is_visible) {
-        element.style.display = (is_visible ? "block" : "none");
-    }
-
     const current_banner_element = document.getElementById("current-banner-container");
     const previous_banner_element = document.getElementById("previous-banners-container");
     const standard_banner_element = document.getElementById("standard-banner-container");
@@ -58,8 +57,9 @@ const banners = characters.filter((c) => c.hasBanner).map((c) => c.name);
         toggleElement(previous_banner_element, false);
         toggleElement(standard_banner_element, true);
     });
+    toggleElement(current_banner_element, false);
     toggleElement(previous_banner_element, false);
-    toggleElement(standard_banner_element, false);
+    toggleElement(standard_banner_element, true);
 }
 
 
@@ -102,31 +102,140 @@ const banners = characters.filter((c) => c.hasBanner).map((c) => c.name);
         
     });
     previous_button.addEventListener("click", () => {
-        if (iterator === 1) {
+        if (iterator === 1)
             toggleButton(previous_button, false);
-            return;
+        else {
+            toggleButton(next_button, true);
+            setBannerDetail(banner_elements, banners[--iterator]);
         }
-
-        toggleButton(next_button, true);
-        setBannerDetail(banner_elements, banners[--iterator]);
     });
     next_button.addEventListener("click", () => {
-        if (iterator === banners.length - 1) {
+        if (iterator === banners.length - 1)
             toggleButton(next_button, false);
-            return;
+        else {
+            toggleButton(previous_button, true);
+            setBannerDetail(banner_elements, banners[++iterator]);
         }
-
-        toggleButton(previous_button, true);
-        setBannerDetail(banner_elements, banners[++iterator]);
     });
 }
 
 
 const standard_single_button = document.getElementById("standard-single-pull-button");
 const standard_multi_button = document.getElementById("standard-multi-pull-button");
-
 standard_single_button.addEventListener("click", () => window.location.replace("gacha.html?singlepull"));
 standard_multi_button.addEventListener("click", () => window.location.replace("gacha.html"));
 
 
-console.log(getWishList());
+{
+    // Wishlist section.
+    const wishlist = getWishList();
+    const max_wishlist_length = 5;
+    const charFilter = (c, manufacturer) => c.isInStandardPool && c.manufacturer === manufacturer && 
+        c.rarity === Character.Rarities.SSR && !c.overspec;
+
+    const manufacturers = {
+        Elysion: {
+            Name: Character.Manufacturers.Elysion,
+            Container: document.getElementById("wishlist-elysion"),
+            WishListImageElements: document.getElementById("wishlist-elysion")
+                .getElementsByClassName("character-display")[0].getElementsByClassName("wishlist-character-image"),
+            WishableCharacters: characters.filter((c) => charFilter(c, Character.Manufacturers.Elysion)),
+            CurrentlyWishedCharacters: wishlist.Elysion
+        },
+        Tetra: {
+            Name: Character.Manufacturers.Tetra,
+            Container: document.getElementById("wishlist-tetra"),
+            WishListImageElements: document.getElementById("wishlist-tetra")
+                .getElementsByClassName("character-display")[0].getElementsByClassName("wishlist-character-image"),
+            WishableCharacters: characters.filter((c) => charFilter(c, Character.Manufacturers.Tetra)),
+            CurrentlyWishedCharacters: wishlist.Tetra
+        },
+        Missilis: {
+            Name: Character.Manufacturers.Missilis,
+            Container: document.getElementById("wishlist-missilis"),
+            WishListImageElements: document.getElementById("wishlist-missilis")
+                .getElementsByClassName("character-display")[0].getElementsByClassName("wishlist-character-image"),
+            WishableCharacters: characters.filter((c) => charFilter(c, Character.Manufacturers.Missilis)),
+            CurrentlyWishedCharacters: wishlist.Missilis
+        },
+        PilgrimOverspec: {
+            Name: Character.Manufacturers.Pilgrim,
+            Container: document.getElementById("wishlist-pilgrim-overspec"),
+            WishListImageElements: document.getElementById("wishlist-pilgrim-overspec")
+                .getElementsByClassName("character-display")[0].getElementsByClassName("wishlist-character-image"),
+            WishableCharacters: characters.filter((c) => c.isInStandardPool && c.rarity === Character.Rarities.SSR && 
+                (c.manufacturer === Character.Manufacturers.Pilgrim || c.overspec)),
+            CurrentlyWishedCharacters: wishlist.Pilgrim
+        }
+    };
+
+    const assignCharactersToList = (manufacturer) => {
+        const profile_prefix = "images/character/profile";
+        
+        for (let i = 0; i < max_wishlist_length; i++) {
+            const wished_character_name = manufacturer.CurrentlyWishedCharacters[i];
+            const banner_name = characters.find((c) => c.name === wished_character_name).bannerName;
+            
+            setImage(manufacturer.WishListImageElements[i], `${profile_prefix}/${banner_name}.png`, wished_character_name);
+        }
+    };
+
+    const character_selector = {
+        SelectedManufacturer: "",
+        IsOpen: false,
+        Container: document.getElementById("wishlist-character-selector"),
+        SelectedCharacters: []
+    };
+    const deleteProfileElements = () => Array.from(character_selector.Container.getElementsByTagName("img")).forEach((p) => p.remove());
+    const createProfileElements = (manufacturer) => manufacturer.WishableCharacters.forEach((c) => {
+        let clicked_before = false;
+        let img = new Image();
+
+        img.addEventListener("click", () => {
+            if (clicked_before) {
+                img.style.backgroundColor = "rgb(164, 211, 252)";
+                img.style.opacity = 1;
+                character_selector.SelectedCharacters = character_selector.SelectedCharacters.filter((sc) => sc !== c.name);
+            }
+            else if (character_selector.SelectedCharacters.length < max_wishlist_length) {                
+                img.style.backgroundColor = "green";
+                img.style.opacity = 0.5;
+                character_selector.SelectedCharacters.push(c.name);
+            }
+            clicked_before = !clicked_before;
+        });
+
+        setImage(img, `images/character/${c.characterProfileImage}`, c.name);
+        character_selector.Container.appendChild(img);
+    });
+    const toggleSelector = (manufacturer) => {
+        character_selector.IsOpen = !character_selector.IsOpen;
+        
+        if (!character_selector.IsOpen) {
+            deleteProfileElements();
+            
+            if (character_selector.SelectedManufacturer === manufacturer.Name && character_selector.SelectedCharacters.length === max_wishlist_length) {
+                manufacturer.CurrentlyWishedCharacters = character_selector.SelectedCharacters;
+                assignCharactersToList(manufacturer);
+                setWishList({
+                    Pilgrim:  manufacturers.PilgrimOverspec.CurrentlyWishedCharacters,
+                    Elysion:  manufacturers.Elysion.CurrentlyWishedCharacters,
+                    Missilis: manufacturers.Missilis.CurrentlyWishedCharacters,
+                    Tetra:    manufacturers.Tetra.CurrentlyWishedCharacters
+                });
+            }
+            character_selector.SelectedManufacturer = "";
+        }
+        else {
+            createProfileElements(manufacturer);
+            character_selector.Container.scrollIntoView();
+            character_selector.SelectedManufacturer = manufacturer.Name;
+        }
+        character_selector.SelectedCharacters = [];
+    };
+
+    [manufacturers.Elysion, manufacturers.Tetra, manufacturers.Missilis, manufacturers.PilgrimOverspec].forEach((m) => {
+        m.Container.addEventListener("click", () => toggleSelector(m));
+        assignCharactersToList(m);
+    });
+}
